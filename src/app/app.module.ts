bde98bb3611635dabfdb55643ae0e177b97db042
Franklin <franklin.op@hotmail.com>
2024-06-24 10:50:21 +0200

    creating sign up, loging and training components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { SignupComponent } from "./auth/signup/signup.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { TrainingComponent } from "./training/training.component";
 
 
 import { AppComponent } from "./app.component";
@@ -8,7 +11,7 @@ import { MaterialModule } from "./material.module";
 
 @NgModule({
     declarations: [
-        
+    
     ],
     imports: [
         BrowserModule,
